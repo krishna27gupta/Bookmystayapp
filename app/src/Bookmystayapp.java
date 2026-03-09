e68fddec97aa727807899fdab56c3e852ebb4be6
mattakarthikeya1232 <mattakarthikeya123@gmail.com>
2026-03-09 14:44:01 +0530

    [krishna] Added [feature/UC1-WelcomePage]

---
+++ app/src/Bookmystayapp.java
@@ -0,0 +1,12 @@
+/**
+ * Entry point for the Hotel Booking System
+ * @author YourName
+ * @version 1.0
+ */
+public class Bookmystayapp {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hotel Booking System v1.0");
+    }
+
+}
